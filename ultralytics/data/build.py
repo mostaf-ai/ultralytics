80658e888aed7629f1443602960903ee5dffa021
mostafamoradipour <mostafa.moradipoor73@gmail.com>
2025-12-22 09:06:39 +0000

    fix(data): enable augmentation for all tasks in build_yolo_dataset function
    - Updated the augmentation parameter in the build_yolo_dataset function to always enable augmentation for training mode, including the pose3d task, ensuring consistent data processing across all tasks.

--- ultralytics/data/build.py
+++ ultralytics/data/build.py
@@ -237,7 +237,7 @@ def build_yolo_dataset(
         img_path=img_path,
         imgsz=cfg.imgsz,
         batch_size=batch,
-        augment=(mode == "train") & (cfg.task != "pose3d"),  # augmentation
+        augment=(mode == "train"),  # augmentation enabled for all tasks including pose3d
         hyp=cfg,  # TODO: probably add a get_hyps_from_cfg function
         rect=cfg.rect or rect,  # rectangular batches
         cache=cfg.cache or None,
